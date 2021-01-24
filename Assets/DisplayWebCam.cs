75649e4412b5ac10acc9f930aba5da3aaf277ee2
Max <max.heimbrock@gmx.de>
2021-01-24 20:21:01 +0100

    passing return value references to opencv
    removed .idea files from tracking

--- Assets/DisplayWebCam.cs
+++ Assets/DisplayWebCam.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class DisplayWebCam : MonoBehaviour
 {
     [DllImport("OpenCV_UnityPlugin")]
-    private static extern void ProcessImage(ref Color32[] rawImage, int width, int height);
+    private static extern void ProcessImage(ref Color32[] rawImage, int width, int height, ref Matrix4x4 camera_pos_result, ref Vector2 ufo_pos_result, ref Vector2 laser_pos_result);
     
     public Image CameraImage;
     
@@ -24,13 +24,21 @@ public class DisplayWebCam : MonoBehaviour
 
     void Update()
     {
+        Matrix4x4 camera_pos_result = new Matrix4x4();
+        Vector2 ufo_pos_result = new Vector2(0,0);
+        Vector2 laser_pos_result = new Vector2(0,0);
+    
         if (_webcam.isPlaying)
         {
             var rawImage = _webcam.GetPixels32();
-            ProcessImage(ref rawImage, _webcam.width, _webcam.height);
+            ProcessImage(ref rawImage, _webcam.width, _webcam.height, ref camera_pos_result, ref ufo_pos_result, ref laser_pos_result);
             _cameraTexture.SetPixels32(rawImage);
             _cameraTexture.Apply();
         }
+        
+        print(camera_pos_result);
+        print(ufo_pos_result);
+        print(laser_pos_result);
     }
 
 }
